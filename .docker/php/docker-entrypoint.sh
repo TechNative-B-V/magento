5ca5820eaaeabef6382fdab668cb398de6588395
ginokok1996 <ginokok1996@hotmail.com>
2022-03-15 11:42:44 +0100

    2fa auth switch

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -28,36 +28,38 @@ mkdir /app/vendor
 
 composer install --no-progress --no-interaction
 
-bin/magento setup:install \
-  --db-host=db \
-  --db-name=$MYSQL_DATABASE \
-  --db-user=$MYSQL_USER \
-  --db-password=$MYSQL_PASSWORD \
-  --admin-firstname=$ADMIN_FIRST_NAME \
-  --admin-lastname=$ADMIN_LAST_NAME \
-  --admin-email=$ADMIN_EMAIL \
-  --admin-user=$ADMIN_USER \
-  --admin-password=$ADMIN_PASSWORD \
-  --use-rewrites=$USE_REWRITES \
-  --elasticsearch-host=$ELASTIC_SEARCH_HOST \
-  --elasticsearch-port=$ELASTIC_SEARCH_PORT \
-  --session-save=$SESSION_SAVE \
-  --use-secure=$USE_SECURE \
-  --use-secure-$USE_SECURE_ADMIN \
-  --backend-frontname=$BACKEND_FRONTNAME \
-  --base-url=$BASE_URL \
-  --base-url-secure=$BASE_URL_SECURE
+# bin/magento setup:install \
+#   --db-host=db \
+#   --db-name=$MYSQL_DATABASE \
+#   --db-user=$MYSQL_USER \
+#   --db-password=$MYSQL_PASSWORD \
+#   --admin-firstname=$ADMIN_FIRST_NAME \
+#   --admin-lastname=$ADMIN_LAST_NAME \
+#   --admin-email=$ADMIN_EMAIL \
+#   --admin-user=$ADMIN_USER \
+#   --admin-password=$ADMIN_PASSWORD \
+#   --use-rewrites=$USE_REWRITES \
+#   --elasticsearch-host=$ELASTIC_SEARCH_HOST \
+#   --elasticsearch-port=$ELASTIC_SEARCH_PORT \
+#   --session-save=$SESSION_SAVE \
+#   --use-secure=$USE_SECURE \
+#   --use-secure-$USE_SECURE_ADMIN \
+#   --backend-frontname=$BACKEND_FRONTNAME \
+#   --base-url=$BASE_URL \
+#   --base-url-secure=$BASE_URL_SECURE
 
 # bin/magento setup:static-content:deploy -f
 
-#check if 2fa needs to be enabled or disabled
-if ["$TWO_FACTOR_AUTH" = true]; then
-  bin/magento module:enable "Magento_TwoFactorAuth"
-else
-  bin/magento module:disable "Magento_TwoFactorAuth"
-fi
+# #check if 2fa needs to be enabled or disabled
+# if [ $TWO_FACTOR_AUTH -eq 1 ]; then
+#   bin/magento module:enable "Magento_TwoFactorAuth"
+# else
+#   bin/magento module:disable "Magento_TwoFactorAuth"
+# fi
 
-#flush magento cache
-bin/magento cache:flush
+# #flush magento cache
+# bin/magento cache:flush
+
+bin/magento config:show
 
 exec "$@"
